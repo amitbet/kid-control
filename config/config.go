2bb8ce431e9c3b2410d393f1fc6fc30db1c9e211
amit b <amitbet@gmail.com>
2018-08-05 02:38:13 +0300

    cleaning

--- config/config.go
+++ config/config.go
@@ -11,12 +11,7 @@ func GetDefaultConfig() *Config {
 	if dConfig == nil {
 		dConfig = &Config{
 			ListeningAddress: ":7777",
-			ServiceList: map[string]string{
-				"home": "http://192.168.1.101:7777/",
-				// "kids":      "http://192.168.1.104:7777/",
-				// "bedroom":   "http://192.168.1.179:7777/",
-				// "localhost": "http://127.0.0.1:7777/",
-			},
+			ServiceList:      map[string]string{},
 		}
 	}
 
